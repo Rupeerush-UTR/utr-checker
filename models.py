452bce2eb0a95cc31fe924a222f9569aa8cb82fb
Rupeerush-UTR <rupeelink.com@gmail.com>
2025-07-31 12:37:35 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+# models.py
+
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -5,7 +7,7 @@ db = SQLAlchemy()
 
 class UTR(db.Model):
     __tablename__ = 'utr_record'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     utr = db.Column(db.String(100), unique=True, nullable=False)
     note = db.Column(db.String(200), default='')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
