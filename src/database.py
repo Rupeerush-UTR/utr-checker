968813899fc970daca2624fa24a447460e972b36
Rupeerush-UTR <rupeelink.com@gmail.com>
2025-08-01 11:32:19 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,23 +1,12 @@
-# database.py
-
 from models import db, UTR
-from datetime import datetime
-#from main import app  # 导入 Flask app 对象
 
-def add_utr(utr_number, note):
-    with app.app_context():
-        exists = UTR.query.filter_by(utr=utr_number).first()
-        if exists:
-            return False  # 已存在
-        new_record = UTR(
-            utr=utr_number,
-            note=note,
-            created_at=datetime.now()
-        )
-        db.session.add(new_record)
-        db.session.commit()
-        return True
+def add_utr(utr, note=""):
+    if UTR.query.filter_by(utr=utr).first():
+        return False
+    new_utr = UTR(utr=utr, note=note)
+    db.session.add(new_utr)
+    db.session.commit()
+    return True
 
-def query_utr(utr_number):
-    with app.app_context():
-        return UTR.query.filter_by(utr=utr_number).first()
+def query_utr(utr):
+    return UTR.query.filter_by(utr=utr).first()
